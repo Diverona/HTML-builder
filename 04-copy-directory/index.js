290f5f9e5aab8fb1c10661d80f9a85b215379121
Kateryna Tonapetian <kattonapetian@gmail.com>
2023-05-06 10:56:38 +0300

    feat: solve the task "04 copy directory"

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require("fs").promises;
+const path = require("path");
 
+const srcFolderPath = path.join(__dirname, "files");
+const destFolderPath = path.join(__dirname, "files-copy");
+
+async function copyDir(src, dest) {
+  try {
+    await fs.mkdir(dest, { recursive: true });
+
+    const items = await fs.readdir(src, { withFileTypes: true });
+
+    for (const item of items) {
+      const srcPath = path.join(src, item.name);
+      const destPath = path.join(dest, item.name);
+
+      if (item.isFile()) {
+        await fs.copyFile(srcPath, destPath);
+      } else if (item.isDirectory()) {
+        await copyDir(srcPath, destPath);
+      }
+    }
+  } catch (err) {
+    console.error("Произошла ошибка при копировании папки:", err);
+  }
+}
+
+copyDir(srcFolderPath, destFolderPath);

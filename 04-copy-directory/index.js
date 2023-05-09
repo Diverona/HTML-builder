2e44688631e48110f7faa241aff5e33a021a33aa
Kateryna Tonapetian <kattonapetian@gmail.com>
2023-05-09 13:30:26 +0300

    fix: fix the problem with updating "files-copy" folder after removing files in task "04 copy directory"

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,13 +4,33 @@ const path = require("path");
 const srcFolderPath = path.join(__dirname, "files");
 const destFolderPath = path.join(__dirname, "files-copy");
 
+async function deleteExtraFiles(src, dest) {
+  const itemsDest = await fs.readdir(dest, { withFileTypes: true });
+
+  for (const item of itemsDest) {
+    const srcPath = path.join(src, item.name);
+    const destPath = path.join(dest, item.name);
+
+    try {
+      await fs.access(srcPath);
+    } catch {
+      if (item.isFile()) {
+        await fs.unlink(destPath);
+      } else if (item.isDirectory()) {
+        await deleteExtraFiles(srcPath, destPath);
+        await fs.rmdir(destPath);
+      }
+    }
+  }
+}
+
 async function copyDir(src, dest) {
   try {
     await fs.mkdir(dest, { recursive: true });
 
-    const items = await fs.readdir(src, { withFileTypes: true });
+    const itemsSrc = await fs.readdir(src, { withFileTypes: true });
 
-    for (const item of items) {
+    for (const item of itemsSrc) {
       const srcPath = path.join(src, item.name);
       const destPath = path.join(dest, item.name);
 
@@ -20,6 +40,8 @@ async function copyDir(src, dest) {
         await copyDir(srcPath, destPath);
       }
     }
+
+    await deleteExtraFiles(srcFolderPath, destFolderPath);
   } catch (err) {
     console.error("Произошла ошибка при копировании папки:", err);
   }
